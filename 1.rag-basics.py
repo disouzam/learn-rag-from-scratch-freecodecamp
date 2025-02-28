60a03d2142e3caf4fb6310017f061e6461f4e860
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-02-27 23:31:47 -0300

    Some adjustments to use OpenAI - still with permissions / API key errors

--- 1.rag-basics.py
+++ 1.rag-basics.py
@@ -39,6 +39,16 @@ def _(mo):
     return
 
 
+@app.cell
+def _():
+    import os
+    os.environ['LANGCHAIN_TRACING_V2'] = 'true'
+    os.environ['LANGCHAIN_ENDPOINT'] = 'https://api.smith.langchain.com'
+    os.environ['LANGCHAIN_API_KEY'] = '<your-api-key>'
+    os.environ['OPENAI_API_KEY'] = '<your-openai-api-key>'
+    return (os,)
+
+
 @app.cell(hide_code=True)
 def _(mo):
     mo.md(
@@ -53,11 +63,56 @@ def _(mo):
 
 @app.cell
 def _():
-    import os
-    os.environ['LANGCHAIN_TRACING_V2'] = 'true'
-    os.environ['LANGCHAIN_ENDPOINT'] = 'https://api.smith.langchain.com'
-    os.environ['LANGCHAIN_API_KEY'] = '<your-api-key>'
-    return (os,)
+    import bs4
+    from langchain import hub
+    from langchain_text_splitters import RecursiveCharacterTextSplitter
+    from langchain_community.document_loaders import WebBaseLoader
+    from langchain_community.vectorstores import Chroma
+    from langchain_core.output_parsers import StrOutputParser
+    from langchain_core.runnables import RunnablePassthrough
+    from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+
+    ### INDEXING
+
+    # Load Documents
+    loader = WebBaseLoader(
+        web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+        bs_kwargs=dict(
+            parse_only=bs4.SoupStrainer(
+                class_=("post-content", "post-title", "post-header")
+            )
+        ),
+    )
+    docs = loader.load()
+
+    # Split
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    splits=text_splitter.split_documents(docs)
+
+    # Embed
+    vectorstore = Chroma.from_documents(documents=splits,
+                                       embedding=OpenAIEmbeddings(
+
+                                       ))
+
+    retriever = vectorstore.as_retriever()
+    return (
+        ChatOpenAI,
+        Chroma,
+        OpenAIEmbeddings,
+        RecursiveCharacterTextSplitter,
+        RunnablePassthrough,
+        StrOutputParser,
+        WebBaseLoader,
+        bs4,
+        docs,
+        hub,
+        loader,
+        retriever,
+        splits,
+        text_splitter,
+        vectorstore,
+    )
 
 
 if __name__ == "__main__":
